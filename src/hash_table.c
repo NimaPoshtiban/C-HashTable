699ee3348f0e2e705675b7b8ecfc7d3027692cf9
Nima Poshtiban <poshtibannima@gmail.com>
2022-04-01 14:28:51 +0430

    added hash table methods

--- src/hash_table.c
+++ src/hash_table.c
@@ -3,9 +3,9 @@
 #include <math.h>
 #include "hash_table.h"
 
+static ht_item HT_DELETED_ITEM = {NULL, NULL};
 int HT_PRIME_1 = 163;
 int HT_PRIME_2 = 199;
-
 //  initialisation functions for ht_item
 // This function allocates a chunk of memory the size of an ht_item,
 // and saves a copy of the strings k and v in the new chunk of memory
@@ -70,4 +70,77 @@ static int ht_get_hash(const char *s, const int num_buckets, const int attempt)
     const int hash_a = ht_hash(s, HT_PRIME_1, num_buckets);
     const int hash_b = ht_hash(s, HT_PRIME_2, num_buckets);
     return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
+}
+
+// inserting item into hash table
+void ht_insert(ht_hash_table *ht, const char *key, const char *value)
+{
+    ht_item *item = ht_new_item(key, value);
+    int index = ht_get_hash(item->key, ht->size, 0);
+    ht_item *current_item = ht->items[index];
+    int i = 1;
+    while (current_item != NULL)
+    {
+        if (current_item != &HT_DELETED_ITEM)
+        {
+            // check if the item already exist in the hash table
+            if (strcmp(current_item->key, key) == 0)
+            {
+                ht_del_item(current_item);
+                ht->items[index] = item;
+                return;
+            }
+            index = ht_get_hash(item->key, ht->size, i);
+            current_item = ht->items[i];
+            i++;
+        }
+    }
+    ht->items[index] = item;
+    ht->count++;
+}
+
+// searching for a value in the hash table
+// returns null if no value found
+char *ht_search(ht_hash_table *ht, const char *key)
+{
+    int index = ht_get_hash(key, ht->size, 0);
+    ht_item *item = ht->items[index];
+    int i = 1;
+    while (item != NULL)
+    {
+        if (item != &HT_DELETED_ITEM)
+        {
+            if (strcmp(key, item->key) == 0)
+            {
+                return item->value;
+            }
+        }
+        index = ht_get_hash(key, ht->size, i);
+        item = ht->items[index];
+        i++;
+    }
+    return NULL;
+}
+
+// deleting item from hashTable
+void ht_delete(ht_hash_table *ht, const char *key)
+{
+    int index = ht_get_hash(key, ht->size, 0);
+    ht_item *item = ht->items[index];
+    int i = 1;
+    while (item != NULL)
+    {
+        if (item != &HT_DELETED_ITEM)
+        {
+            if (strcmp(item->key, key) == 0)
+            {
+                ht_del_item(item);
+                ht->items[index] = &HT_DELETED_ITEM;
+            }
+        }
+        index = ht_get_hash(key, ht->size, i);
+        item = ht->items[index];
+        i++;
+    }
+    ht->count--;
 }
